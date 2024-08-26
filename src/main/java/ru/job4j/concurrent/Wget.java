e3ab7d95ea504c8207bb348857abd482a8dd1d61
selov <selove.jara@gmail.com>
2024-08-26 15:30:59 +0400

    4. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,18 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        for (int index = 0; index <= 100; index++) {
+                            Thread.sleep(1000);
+                            System.out.print("\rLoading : " + index + "%");
+                        }
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+        thread.start();
+    }
+}
