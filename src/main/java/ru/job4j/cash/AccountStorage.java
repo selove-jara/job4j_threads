c950cc5a52177c1fcfabe1c4725a0beee17c1860
selov <selove.jara@gmail.com>
2024-08-31 12:27:46 +0400

    3. Денежные переводы AccountStorage [#1104]

---
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -0,0 +1,41 @@
+package ru.job4j.cash;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+@ThreadSafe
+public class AccountStorage {
+    @GuardedBy("this")
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
+
+    public synchronized boolean add(Account account) {
+        return accounts.putIfAbsent(account.id(), account) == null;
+    }
+
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(), account) != null;
+    }
+
+    public synchronized void delete(int id) {
+        accounts.remove(id);
+    }
+
+    public synchronized Optional<Account> getById(int id) {
+        return Optional.ofNullable(accounts.get(id));
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        boolean result = false;
+        Account fromAccount = accounts.get(fromId);
+        Account toAccount = accounts.get(toId);
+        if (fromAccount != null || toAccount != null || fromAccount.amount() > amount) {
+            update(new Account(fromId, fromAccount.amount() - amount));
+            update(new Account(toId, toAccount.amount() + amount));
+            result = true;
+        }
+        return result;
+    }
+}
