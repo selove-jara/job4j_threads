cc416dc9f05edc28f7c36a05b41b76d2d424631d
selov <selove.jara@gmail.com>
2024-09-02 23:37:43 +0400

    Изменение

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -6,7 +6,6 @@ public class ConcurrentOutput {
                 () -> System.out.println(Thread.currentThread().getName())
         );
         another.start();
-
         Thread second = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
